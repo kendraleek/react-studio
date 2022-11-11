2fe1f767024183e94a573eb844c160b670315832
kendraleek <kendralee@kendras-air-2.devices.brown.edu>
2022-11-10 20:48:53 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,34 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cartItems, setCartItems] = useState([])
 
+  function addToCart(item) {
+    // cartItems is an array the ... expands the array to give individual ele and 
+    // appending new item to end of array
+    setCartItems([...cartItems, item])
+  }
+  function grandTotal(items) {
+    const myPrices = items.map(item => item.price)
+    const sum = myPrices.reduce((price, item) => price + item, 0)
+    return sum
+  }
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div className="left">
+        <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+        <div className="bi">
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            <BakeryItem details={item} key={index} addToCart={addToCart}/>
+            // <p>Bakery Item {index}</p> // replace with BakeryItem component
+          ))} 
+        </div>
+      </div>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
+      <div className ="right">
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cartItems.map((item, index) => <p>{item.name}</p>)}
+        <p>Grand total = {grandTotal(cartItems)}</p>
       </div>
     </div>
   );
